a9e1783befbfae0b97260856d356ee7d65929352
poet.me <=poet.me@kakaocorp.com>
2018-04-12 11:34:54 +0900

    add chap03 match expression exercise

--- src/main/scala/net/bluepoet/chap03/MatchExpression.scala
+++ src/main/scala/net/bluepoet/chap03/MatchExpression.scala
@@ -2,6 +2,13 @@ package net.bluepoet.chap03
 
 object MatchExpression {
   def main(args: Array[String]): Unit = {
+    val s = "test"
 
+    val response = s match {
+      case "test" => "ok"
+      case other => "no"
+    }
+
+    println(response)
   }
 }
